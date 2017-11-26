81b063c759ca138e9fa9d8fb0b4e4716894c7f35
xanderflood <xander.t.flood@gmail.com>
2017-11-25 23:43:09 -0500

    Created factories for existing models

---
+++ factories/cultures.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :culture do
+    name "Lion's Mane - Cascade Springs"
+    species "Hericium Erinaceus"
+    collected "2017-11-24"
+    comments "Found on a rotting log at Cascade Springs Nature Preserve, about 3 in diameter"
+  end
+end
