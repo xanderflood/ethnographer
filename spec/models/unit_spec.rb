a2d2520926936d59026071bc3d8f5d7b546ea774
xanderflood <xander.t.flood@gmail.com>
2017-12-04 21:15:17 -0500

    Stubbed specs for Unit model

--- spec/models/unit_spec.rb
+++ spec/models/unit_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Unit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "uuid" do
+    it "should have the correct form of uuid when blank"
+    it "should have the correct form of uuid when innoculated"
+  end
+
+  describe "generation" do
+    it "should return 0 for a Unit without a parent"
+    it "should always be 1 greater than the generation of its parent"
+  end
 end
