113aa88a093425e47573c7d8d6bdc0e4bc4c0ad5
Xander Flood <xander.t.flood@gmail.com>
2018-01-25 01:02:20 +0000

    Put tree SQL into a concern

---
+++ app/models/concerns/treeable.rb
@@ -0,0 +1,82 @@
+module Treeable
+  extend ActiveSupport::Concern
+
+  included do
+    default_scope -> { order(:parent_id, :id) }
+    scope :initial, -> { where(parent: nil) }
+
+    belongs_to :parent, class_name: name, required: false
+    has_many :children, class_name: name, foreign_key: :parent_id
+  end
+
+  def initial?; parent.nil?; end
+
+  def generation
+    if initial?
+      0
+    elsif !persisted?
+      self.parent.generation + 1
+    else
+      self.ancestors.count - 1
+    end
+  end
+
+  # these all require the unit to be persisted
+  def descendents
+    self.class.where("#{self.class.table_name}.id IN (#{self.class.descendants_sql_for(self)})")
+  end
+
+  def ancestors
+    self.class.where("#{self.class.table_name}.id IN (#{self.class.ancestors_sql_for(self)})")
+  end
+
+  class_methods do
+    # declare attributes that should be inherited from the parent record
+    def inherits *attributes
+      before_save :inherit_attributes
+
+      define_method(:inherit_attributes) do
+        return if self.initial?
+
+        attributes.each do |attrib|
+          self.write_attribute(attrib, self.parent[attrib])
+        end
+      end
+    end
+
+    ### SQL queries ###
+    def descendants_sql_for(instance)
+      # taken from https://hashrocket.com/blog/posts/recursive-sql-in-activerecord
+      <<-SQL
+      WITH RECURSIVE search_tree(id, path) AS (
+          SELECT id, ARRAY[id]
+          FROM #{table_name}
+          WHERE id = #{instance.id}
+        UNION ALL
+          SELECT #{table_name}.id, path || #{table_name}.id
+          FROM search_tree
+          JOIN #{table_name} ON #{table_name}.parent_id = search_tree.id
+          WHERE NOT #{table_name}.id = ANY(path)
+      )
+      SELECT id FROM search_tree ORDER BY path
+      SQL
+    end
+
+    def ancestors_sql_for(instance)
+      # adapted from https://hashrocket.com/blog/posts/recursive-sql-in-activerecord
+      <<-SQL
+      WITH RECURSIVE search_tree(id, pid) AS (
+          SELECT id, parent_id
+          FROM #{table_name}
+          WHERE id = #{instance.id}
+        UNION ALL
+          SELECT #{table_name}.id, #{table_name}.parent_id
+          FROM search_tree
+          JOIN #{table_name} ON #{table_name}.id = search_tree.pid
+          WHERE NOT #{table_name}.id = #{instance.id}
+      )
+      SELECT id FROM search_tree
+      SQL
+    end
+  end
+end
--- app/models/unit.rb
+++ app/models/unit.rb
@@ -2,28 +2,15 @@ class Unit < ApplicationRecord
   # TODO: selectors for these
   belongs_to :medium
   belongs_to :culture
-  belongs_to :parent, class_name: :Unit, required: false
-  has_many :children, class_name: :Unit, foreign_key: :parent_id
 
   before_create :set_uuid
-  before_save   :set_culture
 
   # if the hash contains an id, this will update
   # if it doesn't, a new one will be created
   accepts_nested_attributes_for :medium
 
-  scope :initial, -> { where(parent: nil) }
-  def initial?; parent.nil?; end
-
-  def generation
-    if initial?
-      0
-    elsif !persisted?
-      self.parent.generation + 1
-    else
-      Unit.ancestral_line_for(self).count - 1
-    end
-  end
+  include Treeable
+  inherits :culture_id
 
   def innoc_date_str
     "" unless self.innoculated?
@@ -32,10 +19,6 @@ class Unit < ApplicationRecord
 
   private
   ### callbacks ###
-  def set_culture
-    self.culture_id = self.parent.culture_id if self.parent.present?
-  end
-
   # TODO: put this in a concern?
   # this is called _before_ saving this unit
   def num_uuuid; Unit.where(parent: self.parent).count; end
@@ -64,52 +47,5 @@ class Unit < ApplicationRecord
       break if num == 0
     end
     str
-  end
-
-  ### class methods ###
-  # TODO: factor these tree queries into a concern, or even a gem
-  # these require the unit to be persisted
-  def self.descendents_tree_for(instance)
-    self.where("#{table_name}.id IN (#{descendants_sql_for(instance)})")
-        .order("#{table_name}.id")
-  end
-
-  def self.ancestral_line_for(instance)
-    self.where("#{table_name}.id IN (#{ancestors_sql_for(instance)})")
-        .order("#{table_name}.id")
-  end
-
-  def self.descendants_sql_for(instance)
-    # taken from https://hashrocket.com/blog/posts/recursive-sql-in-activerecord
-    <<-SQL
-    WITH RECURSIVE search_tree(id, path) AS (
-        SELECT id, ARRAY[id]
-        FROM #{table_name}
-        WHERE id = #{instance.id}
-      UNION ALL
-        SELECT #{table_name}.id, path || #{table_name}.id
-        FROM search_tree
-        JOIN #{table_name} ON #{table_name}.parent_id = search_tree.id
-        WHERE NOT #{table_name}.id = ANY(path)
-    )
-    SELECT id FROM search_tree ORDER BY path
-    SQL
-  end
-
-  def self.ancestors_sql_for(instance)
-    # adapted from https://hashrocket.com/blog/posts/recursive-sql-in-activerecord
-    <<-SQL
-    WITH RECURSIVE search_tree(id, pid) AS (
-        SELECT id, parent_id
-        FROM #{table_name}
-        WHERE id = #{instance.id}
-      UNION ALL
-        SELECT #{table_name}.id, #{table_name}.parent_id
-        FROM search_tree
-        JOIN #{table_name} ON #{table_name}.id = search_tree.pid
-        WHERE NOT #{table_name}.id = #{instance.id}
-    )
-    SELECT id FROM search_tree
-    SQL
   end
 end
